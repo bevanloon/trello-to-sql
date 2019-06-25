b808187ea496f45dda13b864ef969ae7aa0a9509
Bevan Loon <bevan.loon@digital.cabinet-office.gov.uk>
2019-06-25 13:13:21 +0100

    Refactor and return readable boards
    Quick refactor
    
    - create and memoize a Faraday connection to the base api
    - require json
    - create a function to return the jsonified body from the api call
    - create a function to return all the boards
    - create a function to print the board name and id pairs
    - extract the key and token and append that to the path just before we
    call the get

--- grab.rb
+++ grab.rb
@@ -1,9 +1,30 @@
 require 'faraday'
 require './secrets'
+require 'json'
 
 include Secrets
 
-conn = Faraday.new(url: "https://api.trello.com/1")
+def conn
+  @conn ||= Faraday.new(url: "https://api.trello.com/1")
+end
 
-response = conn.get "members/me/boards?key=#{Secrets.key}&token=#{Secrets.token}"
-puts response.body
+def key_and_token
+  "key=#{Secrets.key}&token=#{Secrets.token}"
+end
+
+def json_response(path)
+  path_and_auth = path + "?#{key_and_token}"
+  JSON.parse(conn.get(path_and_auth).body)
+end
+
+def boards
+  json_response "members/me/boards"
+end
+
+def print_boards
+  boards.each do |board|
+    puts "#{board["id"]}, #{board["name"]}"
+  end
+end
+
+print_boards
